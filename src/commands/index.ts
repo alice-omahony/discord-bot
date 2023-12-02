acea9337f7b32619c8c3f52f12173d44134e23ca
Squrrli <aliceomahony98@gmail.com>
2023-12-02 23:20:32 +0100

    add quote command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,12 @@
 import * as ping from "./ping";
 import * as reload_commands from "./reload-commands";
 import * as get_channel_messages from "./get-channel-messages";
+import * as quote from "./quote";
 
 
 export const commands = {
     ping,
     reload_commands,
-    get_channel_messages
+    get_channel_messages,
+    quote
 };
--- src/commands/quote.ts
+++ src/commands/quote.ts
@@ -1,11 +1,9 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
-import { getClientCredentials } from "../backend/auth";
 
 export const data = new SlashCommandBuilder()
-  .setName("ping")
-  .setDescription("Replies with Pong!");
-
+  .setName("quote")
+  .setDescription("Create a quote and save it to the database.");
 
 export async function execute(interaction: CommandInteraction) {
-  return interaction.reply("Pong!");
+  return interaction.reply("Quote created!");
 }
