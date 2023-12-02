7b0430f197714afa86f15abd15353f715d983064
Squrrli <aliceomahony98@gmail.com>
2023-12-02 01:16:15 +0100

    replace mysql with postgres and get first db woking

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
+import { todo } from "node:test";
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
   throw new Error("Missing environment variables");
@@ -12,3 +13,13 @@ export const config = {
   DISCORD_TOKEN,
   DISCORD_CLIENT_ID,
 };
+
+// TODO: Extract this duplication to something better
+
+if (!DB_USER  || !DB_PASSWORD || !DB_HOST || !DB_PORT) {
+  throw new Error("Missing database environment variables");
+}
+
+export const dbConfig = {
+  DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,51 @@
 import { Client } from "discord.js";
+import { Client as PgClient } from "pg";
+
 import { deployCommands } from "./deploy-commands";
 import { commands } from "./commands";
-import { config } from "./config";
-import { startServer } from './backend/server';
+import { config, dbConfig } from "./config";
+import { startServer } from "./backend/server";
 
 
 const client = new Client({
     intents: ["Guilds", "GuildMessages"],
 });
 
-client.once("ready", () => {
+client.once("ready", async () => {
     console.log("Discord bot is ready! 🤖");
     console.log('Starting auth server...');
     startServer();
+
+    const client = new PgClient({
+      database: 'discordbot',
+      host: 'postgres',
+      user: 'postgres',
+      password: 'postgres',
+      port: 5432
+    });
+    client.connect()
+      .then(() => console.log('Connected to PostgreSQL'))
+      .catch(error => console.error('Error connecting to PostgreSQL:', error));
+
+
+      try {
+        const query = {
+          // give the query a unique name
+          name: 'fetch-quote',
+          text: 'SELECT * FROM quote WHERE id = $1',
+          values: [1],
+        }
+
+
+        const res = await client.query(query);
+        console.log(res);
+      } catch (err) {
+          console.error(err);
+      } finally {
+          await client.end()
+      }
+
+
 });
 
 client.on("guildCreate", async (guild) => {
